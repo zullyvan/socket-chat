dbbfdf0b0c55a5d6eaf4482ab0a8b75d449c5a82
Zullyvan <Zullyvan@gmail.com>
2020-07-18 17:38:33 -0500

    Chat finalizado V1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,20 +4,24 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -29,33 +33,30 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/*
-socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//escuchar cambios de usuarios
-//cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
+    //console.log(personas);
 });
 
-
-//Mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mesaje Privado', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 
 });
